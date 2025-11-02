cb83fa727c809227e4e58821c771e53f78d40b5d
muhd-adhil <adhilmuhammed8787@gmail.com>
2025-11-02 12:12:15 +0530

    Challenge 2 add

--- github-challenge/challenge.2.py
+++ github-challenge/challenge.2.py
@@ -1,6 +1,11 @@
-def ad(b):
+def rev(b):
     temp=[]
     for i in b:
         temp.insert(0,i)
-        print()
-        rev(a)
+    # print(f"Reversed list{temp}")
+    print(f" Original list : {b}\n Reversed list : {temp}")
+
+
+
+b=[1,2,3,4,5]
+rev(b)
